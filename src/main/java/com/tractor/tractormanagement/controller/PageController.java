db52f9f8f9eef05adee048223c8909ea1e7a6ccd
ritesh8626 <riteshphalke100@gmail.com>
2026-03-12 20:23:51 +0530

    Update

--- src/main/java/com/tractor/tractormanagement/controller/PageController.java
+++ src/main/java/com/tractor/tractormanagement/controller/PageController.java
@@ -13,7 +13,7 @@ public class PageController {
 
     @GetMapping("/index")
     public String index() {
-        return "index.html";
+        return "index";
     }
 
     @GetMapping("/register")
