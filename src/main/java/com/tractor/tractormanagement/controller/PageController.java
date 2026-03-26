038b7d68a5d840211a443fbc3367c9e16a50662c
ritesh8626 <riteshphalke100@gmail.com>
2026-03-26 15:17:55 +0530

    Update all

--- src/main/java/com/tractor/tractormanagement/controller/PageController.java
+++ src/main/java/com/tractor/tractormanagement/controller/PageController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-   @GetMapping("/register")
-    public String register() {
-        return "register";
+   @GetMapping("/")
+    public String login() {
+        return "login";
     }
 
     @GetMapping("/login")
